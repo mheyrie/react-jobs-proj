55cb6e716768596dfedc3b9117962a3895b75466
Mheyrie <omota4fidelity@gmail.com>
2024-04-07 20:09:14 +0100

    Navbar completed

--- src/Pages/Homes/Navbar.jsx
+++ src/Pages/Homes/Navbar.jsx
@@ -9,13 +9,13 @@ const Navbar = () => {
                 <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
                     <a href="/index.html" className="flex flex-shrink-0 items-center mr-4">
                        <img className="h-10 w-auto rounded-full" src={logor} alt="logo"/>
-                       <span className="hidden md:block text-white text-2xl font-bold ml-2">Available Job OFFERS</span> 
+                       <span className="hidden md:block text-[#00A8C5]  text-2xl font-bold ml-2">Available Job OFFERS</span> 
                     </a>
                     <div className="md:ml-auto">
                         <div className="flex space-x-2">
-                            <a href="/index.html" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</a>
-                            <a href="/jobs.html" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Jobs</a>
-                            <a href="/add-jobs.html" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Add Jobs</a>
+                            <a href="/index.html" className="text-white hover:bg-lightAlabaster hover:text-white rounded-md px-3 py-2">Home</a>
+                            <a href="/jobs.html" className="text-white hover:bg-lightAlabaster hover:text-white rounded-md px-3 py-2">Jobs</a>
+                            <a href="/add-jobs.html" className="text-white hover:bg-lightAlabaster hover:text-white rounded-md px-3 py-2">Add Jobs</a>
                         </div>
                     </div>
                 </div>
