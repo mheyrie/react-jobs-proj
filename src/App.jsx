8e48ee6dc25201acdf12192de16ff0b5a91bf98c
Mheyrie <omota4fidelity@gmail.com>
2024-04-07 02:21:28 +0100

    Creating of pages and logo design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Home from './Pages/Homes/Homescreen'
+import Navbar from "./Pages/Homes/Navbar"
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <Navbar/>
+      <Home/>
+    </div>
   )
 }
 
---
+++ src/Pages/Homes/Homescreen/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Herosection from "../Herosection"
+
+function index() {
+  return (
+    <>
+        <Herosection/>
+    </>
+  )
+}
+
+export default index
