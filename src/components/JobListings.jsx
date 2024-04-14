61da41c0878e5459c89531d946cbe2c070c12e3d
Mheyrie <omota4fidelity@gmail.com>
2024-04-14 15:16:45 +0100

    Added a condition to control the number of jobs shown on Home page and on jobListing page

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -10,8 +10,9 @@ const JobListings = ({isHome = false}) => {
 
   useEffect(() => {
     const fectchJobs = async  () => {
+      const apiUrl = isHome ? 'http://localhost:8000/jobs?_limit=3' : 'http://localhost:8000/jobs'
       try{
-        const res = await fetch('http://localhost:8000/jobs')
+        const res = await fetch(apiUrl)
         const data = await res.json()
         setJobs(data)
       } catch (error){
@@ -19,8 +20,7 @@ const JobListings = ({isHome = false}) => {
       } finally {
         setLoading(false)
       }
-      
-    }
+     }
     fectchJobs();
   }, [])
 
