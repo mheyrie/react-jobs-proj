0686046b9d76563665b5b75c9bca1c8360326a69
Mheyrie <omota4fidelity@gmail.com>
2024-04-16 17:18:09 +0100

    Implement the use of React Toast

--- src/Pages/AddJobPage.jsx
+++ src/Pages/AddJobPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import {toast} from 'react-toastify';
 
 const AddJobPage = ({addJobSubmit}) => {
 
@@ -32,6 +33,7 @@ const AddJobPage = ({addJobSubmit}) => {
             }
         }
         addJobSubmit(newJob)
+        toast.success('Job Added successfully')
         return navigate('/jobs')
     }
 
--- src/Pages/JobPage.jsx
+++ src/Pages/JobPage.jsx
@@ -1,6 +1,7 @@
 // import React, { useEffect, useState } from 'react'
-import {useParams, useLoaderData, useNavigate} from 'react-router-dom'
-import {Link} from 'react-router-dom'
+import {useParams, useLoaderData, useNavigate} from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import {toast} from 'react-toastify';
 
 const JobPage = ({ deleteJob }) => {
 
@@ -14,6 +15,7 @@ const JobPage = ({ deleteJob }) => {
             return;
         }else{
             deleteJob(jobId)
+            toast.success('Job deleted successfully')
         }
         navigate('/jobs')
     }
