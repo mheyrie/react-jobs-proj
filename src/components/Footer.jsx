9ab55ad0a8140eb51eb464ca62c88f9a00c73fa5
Mheyrie <omota4fidelity@gmail.com>
2024-10-25 17:39:16 +0100

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,19 @@
+import { Link } from "react-router-dom";
 import logor from "../assets/images/logor.png";
 
 function Footer() {
   return (
     <div className="bg-neutral-800 w-full text-slate-300 h-24">
-        <div className="flex justify-between p-6">
-            <img src={logor} alt="" className="w-12 rounded-full"/>
-            <p className="text-4xl text-center">This is Footer</p>
-            <p className="text-4xl text-center">This is Footer</p>
-            <p className="text-4xl text-center">This is Footer</p>
-        </div>
-        
+      <div className="flex justify-between p-6">
+        <Link to="/">
+          <img src={logor} alt="" className="w-12 rounded-full" />
+        </Link>
+
+        <p className="text-4xl text-center">This is Footer</p>
+        <p className="text-4xl text-center">This is Footer</p>
+        <p className="text-4xl text-center">This is Footer</p>
+      </div>
     </div>
-  )
+  );
 }
-export default Footer
+export default Footer;
