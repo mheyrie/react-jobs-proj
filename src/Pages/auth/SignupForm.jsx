744af342e47d1308e5d61882982948eeaf71c36e
Mheyrie <omota4fidelity@gmail.com>
2024-10-30 23:07:48 +0100

    Updated

--- src/Pages/auth/SignupForm.jsx
+++ src/Pages/auth/SignupForm.jsx
@@ -1,10 +1,11 @@
 function SignupForm() {
   return (
-    <div className="text-white">
-        <h1>fgugkjjkbj</h1>
-        <h1>fgugkjjkbj</h1>
-        <h1>fgugkjjkbj</h1>
-        <h1>fgugkjjkbj</h1>
+    <div className="text-white bg-gray-400 p-5">
+      <h4 className="">Sign Out</h4>
+      
+       <form action="">
+        <input type="text" placeholder="Enter Email" />
+       </form>
     </div>
   )
 }
